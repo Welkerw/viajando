c3b83552253d363fe3c34a3985c8eaa80c13e9bf
Amanda Karina <am.karina@hotmail.com>
2017-02-28 19:35:28 -0300

    Tokenização das palavras

--- data_analisys/analise.py
+++ data_analisys/analise.py
@@ -1,4 +1,5 @@
 import pymongo
+import nltk
 from pymongo import MongoClient
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
@@ -7,25 +8,30 @@ client = MongoClient()
 client = MongoClient('mongodb://localhost:27017/')
 db = client.viajando
 termos = db.tweets
-result = termos.find().sort("value", -1).limit(15)
+result = termos.find()
 textos = []
+filtrado = []
 for doc in result:
-	textos.append(doc['text'])
+	try:
+		textos.append(doc['text'])
+	except:
+		print("fim do cursor")
+		break
 
+print("Removendo stopwords")
 for texto in textos:
-	print word_tokenize(texto)
-	filtrado = [w for w in word_tokenize(texto) if not w in stopwords.words('portuguese')]
-	print filtrado
+	filtrado += [w for w in word_tokenize(texto) if not w in stopwords.words('portuguese')]
 
-	stemmer = nltk.stem.RSLPStemmer()
-	filt_stem = []
+stemmer = nltk.stem.RSLPStemmer()
+filt_stem = []
 
-	for i in filtrado:
-	 filt_stem.append(stemmer.stem(i))
+print("Stemming - removendo plural, gênero, etc")
+for i in filtrado:
+	filt_stem.append(stemmer.stem(i))
 
-	print filt_stem
+print (filt_stem)
+print("Calculando frequência")
+fdist = nltk.FreqDist(filt_stem)
 
-	fdist = nltk.FreqDist(filt_stem)
-
-	for i in fdist:
-	 print i + ': ' + str(fdist[i])
+for i in fdist:
+	print (i + ': ' + str(fdist[i]))
