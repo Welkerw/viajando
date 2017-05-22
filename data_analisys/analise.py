6cfcc991d6b654db058c5d25d513c8c416ea6121
Amanda Karina <am.karina@hotmail.com>
2017-05-22 20:28:13 -0300

    Adicionando fluxos e saídas da análise de dados

--- data_analisys/analise.py
+++ data_analisys/analise.py
@@ -1,36 +1,62 @@
 import pymongo
 import nltk
+import re
+import time
+import json
 from pymongo import MongoClient
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+
 
 client = MongoClient()
 client = MongoClient('mongodb://localhost:27017/')
 db = client.viajando
 termos = db.tweets
+print("Recuperando Tweets")
+print (time.strftime("%d/%m/%Y %H:%M:%S"))
 result = termos.find()
 textos = []
 filtrado = []
+emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags=re.UNICODE)
+print("removendo emojis, mentions, urls")
+print (time.strftime("%d/%m/%Y %H:%M:%S"))
 for doc in result:
 	try:
-		textos.append(doc['text'])
+		textos.append(emoji_pattern.sub(r'', re.sub(r"(?:\@|https?\://)\S+", '', doc['text'], flags=re.MULTILINE)))
 	except:
 		print("fim do cursor")
 		break
 
 print("Removendo stopwords")
-for texto in textos:
-	filtrado += [w for w in word_tokenize(texto) if not w in stopwords.words('portuguese')]
-
+print (time.strftime("%d/%m/%Y %H:%M:%S"))
+#print("Stemming - removendo plural, gênero, etc")
 stemmer = nltk.stem.RSLPStemmer()
-filt_stem = []
-
-print("Stemming - removendo plural, gênero, etc")
-for i in filtrado:
-	filt_stem.append(stemmer.stem(i))
+for texto in textos:
+	frase = ""
+	for w in word_tokenize(texto) :
+		if not w in stopwords.words('portuguese') and w != "RT":
+			frase += w + " "
+	filtrado += [ frase ]
+print("Fim")
+print (time.strftime("%d/%m/%Y %H:%M:%S"))
 
 print("Calculando frequência")
-fdist = nltk.FreqDist(filt_stem)
+fdist = nltk.FreqDist(filtrado)
+
 
 for i in fdist:
-	print (i + ': ' + str(fdist[i]))
+	if i != "" and i != " ":
+		tweet = {"texto": i, "freq": fdist[i]}
+		print (tweet)
+		db.carnaval_tratado.insert_one(tweet);
+	
+
+
+
+
+
